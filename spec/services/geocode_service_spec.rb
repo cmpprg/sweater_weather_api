826bf1a47bc4280373aef310da954e1480a048fd
Ryan Camp <campryan@comcast.net>
2020-06-06 16:57:17 -0600

    Write test to check return of geocode service

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GeocodeService do
-  VCR.use_cassette('geocode', record: :new_episodes) do
-    it "exists" do
+  it "exists" do
+    service = GeocodeService.new
+
+    expect(service).to be_instance_of GeocodeService
+  end
+
+  it "can serve up a latitude and longitude for a city" do
+    VCR.use_cassette('geocode', record: :new_episodes) do
       city = 'denver'
       state = 'co'
-      service = GeocodeService.new(city, state)
+      service = GeocodeService.new
+      lat_long = service.lat_long(city, state)
 
-      expect(service).to be_instance_of GeocodeService
-    end
-
-    it "can serve up a latitude and longitude for a city" do
-      city = 'denver'
-      state = 'co'
-      service = GeocodeService.new(city, state)
-
-      expect(service.lat_long).to eql({lat: 39.7392358,
-                                       lng: -104.990251})
+      expect(lat_long).to have_key(:results)
+      expect(lat_long[:results].first[:address_components].first[:long_name]).to eql(city.capitalize)
+      expect(lat_long[:results].first[:address_components][2][:short_name]).to eql(state.upcase)
+      expect(lat_long[:results].first[:geometry][:location][:lat]).to eql(39.7392358)
+      expect(lat_long[:results].first[:geometry][:location][:lng]).to eql(-104.990251)
     end
   end
 end
