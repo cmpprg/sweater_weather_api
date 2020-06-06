00b3c9a24111311b5b74ad86f50fef1a38ef3603
Ryan Camp <campryan@comcast.net>
2020-06-06 16:11:02 -0600

    Write test to check that geocode service can return lat and long

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe GeocodeService do
-  it "exists" do
-    city = 'denver'
-    state = 'co'
-    service = GeocodeService.new(city, state)
+  VCR.use_cassette('geocode', record: :new_episodes) do
+    it "exists" do
+      city = 'denver'
+      state = 'co'
+      service = GeocodeService.new(city, state)
 
-    expect(service).to be_instance_of GeocodeService
+      expect(service).to be_instance_of GeocodeService
+    end
+
+    it "can serve up a latitude and longitude for a city" do
+      city = 'denver'
+      state = 'co'
+      service = GeocodeService.new(city, state)
+
+      expect(service.lat_long).to eql({lat: 39.7392358,
+                                       lng: -104.990251})
+    end
   end
 end
