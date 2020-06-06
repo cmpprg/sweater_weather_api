ba019d4b7f2689dd5d40ab4112b2fe77271a9fa6
Ryan Camp <campryan@comcast.net>
2020-06-06 17:10:51 -0600

    Refactor geocode service

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,14 +1,29 @@
 class GeocodeService
   def lat_long(city, state)
-    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
-      req.params['address'] = "#{city}, #{state}"
-      req.params['key'] = ENV['GEOCODE_KEY']
-    end
-
-    hashed_json = JSON.parse(response.body, symbolize_names: true)
-
-    
+    hashed_json(response(city, state))
   end
 
   private
+
+  def base_url
+    'https://maps.googleapis.com/maps/api/geocode'
+  end
+
+  def connect
+    Faraday.new(base_url) do |req|
+      req.params['key'] = ENV['GEOCODE_KEY']
+    end
+  end
+
+  def response(city, state)
+    connect.get('json') do |req|
+      req.params['address'] = "#{city}, #{state}"
+    end
+  end
+
+  def hashed_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+
 end
