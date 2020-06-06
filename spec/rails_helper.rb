86191056bbfc886fd3372fd29ecac637dd4df41c
Ryan Camp <campryan@comcast.net>
2020-06-06 16:10:32 -0600

    Set up webmock and vcr in specs helper files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/apis"
+  config.hook_into :webmock
+end
