edf1ff2e753bfd5de135850634f4e3a227a87fba
Ryan Camp <campryan@comcast.net>
2020-06-06 16:58:09 -0600

    Implement faraday to successfully call to googles geocode api

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,6 +1,14 @@
 class GeocodeService
-  def initialize(city, state)
-    @city = city
-    @state = state
+  def lat_long(city, state)
+    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
+      req.params['address'] = "#{city}, #{state}"
+      req.params['key'] = ENV['GEOCODE_KEY']
+    end
+
+    hashed_json = JSON.parse(response.body, symbolize_names: true)
+
+    
   end
+
+  private
 end
