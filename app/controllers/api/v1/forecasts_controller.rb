320fbd9315292e168930e352343e81582447f399
Ryan Camp <campryan@comcast.net>
2020-06-06 18:57:17 -0600

    Declare objects in show action of forecasts controller

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
+    forecast = Forecast.new(format_location)
+    render json: ForecastSerializer.new(forecast)
+  end
 
+  private
+
+  def format_location
+    {
+      city: split_location_params.first,
+      state: split_location_params.last
+    }
+  end
+
+  def split_location_params
+    params[:location].split(', ')
   end
 end
