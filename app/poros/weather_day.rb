a572fff1bbba8330f009ce3f89fb166be594c931
Ryan Camp <campryan@comcast.net>
2020-06-07 12:18:34 -0600

    Add @amount_of_rain to guard against nil value

--- app/poros/weather_day.rb
+++ app/poros/weather_day.rb
@@ -3,7 +3,7 @@ class WeatherDay
     @day_of_week = day_of_week(daily_info[:dt])
     @icon = icon_url(daily_info[:weather].first[:icon])
     @description_short = daily_info[:weather].first[:main]
-    @rain = daily_info[:rain]
+    @rain = amount_of_rain(daily_info[:rain])
     @high_temp = daily_info[:temp][:max]
     @low_temp = daily_info[:temp][:min]
   end
@@ -16,5 +16,10 @@ class WeatherDay
 
   def icon_url(icon_code)
     "http://openweathermap.org/img/wn/#{icon_code}@2x.png"
+  end
+
+  def amount_of_rain(rain)
+    return 0 if rain.nil?
+    rain
   end
 end
