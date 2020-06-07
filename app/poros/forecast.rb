4ea664ce4f825e8c3d16b9b0ab0c1e30a9452f6a
Ryan Camp <campryan@comcast.net>
2020-06-06 20:53:10 -0600

    Set up forecast and serializer

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,15 +1,29 @@
 class Forecast
+  attr_reader :city_info,
+              :current_weather,
+              :hourly_weather,
+              :daily_weather
+
+
   def initialize(location)
     @city = location[:city]
     @state = location[:state]
-    @city_info = "#{@city}, #{@state}"
+    @city_info = "#{@city.capitalize}, #{@state.upcase}"
     @current_weather = CurrentWeather.new(@weather)
     @hourly_weather = HourlyWeather.new(@weather)
     @daily_weather = DailyWeather.new(@weather)
     @weather ||= weather_info
   end
 
+  private
+
   def weather_info
-    
+    weather_service = OpenWeatherService.new
+    weather_service.weather_data(geo_location.latitude, geo_location.longitude)
+  end
+
+  def geo_location
+    geocode_service = GeocodeService.new
+    Location.new(geocode_service.info_for(@city, @state))
   end
 end
