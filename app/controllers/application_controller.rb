5ad95a5152cd0fd9b60f664850ebd326b0c2cb75
Ryan Camp <campryan@comcast.net>
2020-06-07 16:44:28 -0600

    Move #split_location_params and #format_location to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def format_location
+    {
+      city: split_location_params.first,
+      state: split_location_params.last
+    }
+  end
+
+  def split_location_params
+    params[:location].split(',')
+  end
+
 end
