ce3f401c640b130e8bc68d682acff610bf327ab6
Ryan Camp <campryan@comcast.net>
2020-06-07 17:12:15 -0600

    Add functionality to backgrounds controller to render image url json

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    
+    background = BackgroundImage.new(format_location)
+    render json: BackgroundImageSerializer.new(background)
   end
 end
