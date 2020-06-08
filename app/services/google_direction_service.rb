172df0038e32411d739166bcb4eb610cf19c2bd1
Ryan Camp <campryan@comcast.net>
2020-06-08 11:50:00 -0600

    Refactor google direction service

--- app/services/google_direction_service.rb
+++ app/services/google_direction_service.rb
@@ -1,6 +1,28 @@
 class GoogleDirectionService
   def trip_info_for(origin, destination)
-    direction_response = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=denver,co&destination=pueblo,co&key=#{ENV['GOOGLE_KEY']}")
-    JSON.parse(direction_response.body, symbolize_names: true)
+    hashed_json(trip_info_response(origin, destination))
+  end
+
+  private
+
+  def base_url
+    'https://maps.googleapis.com/maps/api'
+  end
+
+  def connect
+    Faraday.new(base_url) do |req|
+      req.params['key'] = ENV['GOOGLE_KEY']
+    end
+  end
+
+  def trip_info_response(origin, destination)
+    connect.get('directions/json') do |req|
+      req.params['origin'] = origin
+      req.params['destination'] = destination
+    end
+  end
+
+  def hashed_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
