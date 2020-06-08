585e27baa068af350e5d08493baaf66e649a866d
Ryan Camp <campryan@comcast.net>
2020-06-08 17:04:04 -0600

    Implement sad path functionality into users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    new_user = User.create(user_params)
-    render json: UserSerializer.new(new_user), status: :created
+    new_user = User.new(user_params)
+    if new_user.save
+      render json: UserSerializer.new(new_user), status: :created
+    else
+      error = Error.new(new_user)
+      render json: ErrorSerializer.new(error), status: :unprocessable_entity
+    end
   end
 
   def user_params
