89b8c47fff6ba913d2166c357d89a25a56c91ccb
Ryan Camp <campryan@comcast.net>
2020-06-08 16:06:08 -0600

    Add before create to user that sets api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_create :set_api_key
 
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 
   has_secure_password
+
+  private
+
+  def set_api_key
+    self.api_key = ApiKeyGenerator.generate(self.email)
+  end
 end
