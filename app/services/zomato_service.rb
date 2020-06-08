c1be170ed3cb1f4bf5e7968d13672a66d72d3344
Ryan Camp <campryan@comcast.net>
2020-06-08 11:28:56 -0600

    Complete zomato service pre refactor

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -1,9 +1,17 @@
 class ZomatoService
   def city_id_search(lat, long)
+    response = Faraday.get("https://developers.zomato.com/api/v2.1/geocode?lat=#{lat}&lon=#{long}") do |req|
+      req.headers['user-key'] = ENV['ZOMATO_KEY']
+    end
 
+    JSON.parse(response.body, symbolize_names: true)[:location][:city_id]
   end
 
   def restaurant_search(city_id, cuisine)
+    response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{city_id}&entity_type=city&q=#{cuisine}") do |req|
+      req.headers['user-key'] = ENV['ZOMATO_KEY']
+    end
 
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
