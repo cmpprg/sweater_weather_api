36a2db90bceb652793b9c76cbca08c132b360d00
Ryan Camp <campryan@comcast.net>
2020-06-08 09:23:09 -0600

    Write spec to drive development of foodie API request

---
+++ spec/requests/api/v1/foodie_request_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe 'Foodie API' do
+  it "can return forecast and restaurant from two cities and a food type" do
+    VCR.use_cassette('foodie_request') do
+      get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
+
+      expect(response).to be_successful
+
+      json = hash_json(response.body)
+
+      require 'pry'; binding.pry
+
+      expect(json[:data]).to have_key(:id)
+      expect(json[:data][:id]).to be_nil
+
+      expect(json[:data]).to have_key(:type)
+      expect(json[:data]).to have_key(:attributes)
+
+      expect(json[:data][:attributes]).to have_key(:end_location)
+      expect(json[:data][:attributes]).to have_key(:travel_time)
+      expect(json[:data][:attributes]).to have_key(:forecast)
+      expect(json[:data][:attributes]).to have_key(:restaurant)
+
+      expect(json[:data][:attributes][:forecast]).to have_key(:summary)
+      expect(json[:data][:attributes][:forecast]).to have_key(:temperature)
+
+      expect(json[:data][:attributes][:restaurant]).to have_key(:name)
+      expect(json[:data][:attributes][:restaurant]).to have_key(:address)
+
+      expect(json[:data][:attributes][:end_location]).to_not be_nil
+      expect(json[:data][:attributes][:travel_time]).to_not be_nil
+      expect(json[:data][:attributes][:forecast][:summary]).to_not be_nil
+      expect(json[:data][:attributes][:forecast][:temperature]).to_not be_nil
+      expect(json[:data][:attributes][:restaurant][:name]).to_not be_nil
+      expect(json[:data][:attributes][:restaurant][:address]).to_not be_nil
+    end
+  end
+end
