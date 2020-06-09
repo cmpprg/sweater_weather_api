b3e4524575a8f1c285de6b65d5dd6aede0f9d1ca
Ryan Camp <campryan@comcast.net>
2020-06-09 13:55:36 -0600

    Add travel time portion of road trip

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,4 +1,5 @@
 class RoadTrip
+  include Convertable
   attr_reader :origin,
               :destination,
               :travel_time,
@@ -7,7 +8,7 @@ class RoadTrip
   def initialize(road_trip_info)
     @origin = format_city_state(road_trip_info[:origin])
     @destination = format_city_state(road_trip_info[:destination])
-    @travel_time = nil
+    @travel_time = rounded_travel_time
     @arrival_forecast = RoadTripForecast.new()
   end
 
@@ -15,5 +16,18 @@ class RoadTrip
 
   def format_city_state(city_state)
     city_state.sub(',', ', ')
+  end
+
+  def rounded_travel_time
+    seconds = directions_info[:routes].first[:legs].first[:duration][:value]
+    Convertable.seconds_to_rounded_hour(seconds)
+  end
+
+  def directions_info
+    @info ||= google_service.directions_info_for(@origin, @destination)
+  end
+
+  def google_service
+    GoogleService.new
   end
 end
