0c578fe3d929e270f2cbb0219d1f29a7522bffde
Ryan Camp <campryan@comcast.net>
2020-06-09 15:54:38 -0600

    Implement road trip happy json return

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::RoadTripsController < ApplicationController
   def create
+    road_trip = RoadTrip.new(road_trip_params)
+    render json: RoadTripSerializer.new(road_trip)
+  end
 
+  private
+
+  def road_trip_params
+    params.permit(:origin, :destination)
   end
 end
