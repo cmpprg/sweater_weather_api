5e196edf64fa4a0150b8fdd9607d9d6f09bbd6be
Ryan Camp <campryan@comcast.net>
2020-06-09 13:07:48 -0600

    Modify create user helper method

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -3,11 +3,11 @@ module HelperMethods
     JSON.parse(response_body, symbolize_names: true)
   end
 
-  def create_user(email, password)
+  def create_user(email, password, password_confirmation = password)
     User.create({
       email: email,
       password: password,
-      password_confirmation: password
+      password_confirmation: password_confirmation
       })
   end
 end
