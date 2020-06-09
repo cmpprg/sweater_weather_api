eb3ce513065faa23ff6a3b7adec70accd5883c5b
Ryan Camp <campryan@comcast.net>
2020-06-09 13:05:02 -0600

    Add directions response to google service

---
+++ app/services/google_service.rb
@@ -0,0 +1,40 @@
+class GoogleService
+  def geocode_info_for(city, state)
+    hashed_json(geocode_response(city, state))
+  end
+
+  def directions_info_for(origin, destination)
+    hashed_json(directions_response(origin, destination))
+  end
+
+  private
+
+  def base_url
+    'https://maps.googleapis.com/maps/api'
+  end
+
+  def connect
+    Faraday.new(base_url) do |req|
+      req.params['key'] = ENV['GOOGLE_KEY']
+    end
+  end
+
+  def geocode_response(city, state)
+    connect.get('geocode/json') do |req|
+      req.params['address'] = "#{city}, #{state}"
+    end
+  end
+
+  def directions_response(origin, destination)
+    connect.get('directions/json') do |req|
+      req.params['origin'] = origin
+      req.params['destination'] = destination
+    end
+  end
+
+  def hashed_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+
+end
