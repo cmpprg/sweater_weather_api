38a6ab1d2361066dee2e4906df2a5804f8d32b4f
Ryan Camp <campryan@comcast.net>
2020-06-08 20:36:25 -0600

    Write tests to check sad path for session request

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -35,4 +35,46 @@ RSpec.describe 'Sessions API' do
     expect(json[:data][:attributes][:email]).to eql(session_params[:email])
     expect(json[:data][:attributes][:api_key]).to_not be_nil
   end
+
+  it "can return error if users email can not be found" do
+    session_params = {
+      email: "doesnote@example.com",
+      password: "password"
+    }
+
+    post '/api/v1/sessions', params:session_params.to_json, headers: @headers
+
+    expect(response.status).to eql(404)
+
+    json = hash_json(response.body)
+
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data]).to have_key(:attributes)
+
+    expect(json[:data][:attributes]).to have_key(:message)
+
+    expect(json[:data][:attributes][:message]).to eql('Email does not exist in our system, please sign up.')
+  end
+
+  it "can return erro if users password is bad" do
+    session_params = {
+      email: "email@example.com",
+      password: "badpassword"
+    }
+
+    post '/api/v1/sessions', params:session_params.to_json, headers: @headers
+
+    expect(response.status).to eql(401)
+
+    json = hash_json(response.body)
+
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data]).to have_key(:attributes)
+
+    expect(json[:data][:attributes]).to have_key(:message)
+
+    expect(json[:data][:attributes][:message]).to eql('Bad Credentials, You have entered the wrong password.')
+  end
 end
