97efb01675304e4e45f54a4034bea08807e0aa03
Ryan Camp <campryan@comcast.net>
2020-06-09 11:32:16 -0600

    Create route for road trips create

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resource :backgrounds, only: [:show]
       resource :users, only: [:create]
       resource :sessions, only: [:create]
+      resource :road_trip, only: [:create]
     end
   end
 end
