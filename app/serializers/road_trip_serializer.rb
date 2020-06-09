95829b3e5f200ffa82adf5d5c2db2486cd9fd3a2
Ryan Camp <campryan@comcast.net>
2020-06-09 13:05:40 -0600

    Create road trip serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,10 @@
+class RoadTripSerializer
+  include FastJsonapi::ObjectSerializer
+
+  set_id { |id| id = nil }
+
+  attributes :origin,
+             :destination,
+             :travel_time,
+             :arrival_forecast
+end
