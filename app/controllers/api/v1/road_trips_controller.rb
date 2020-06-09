e1b00f9947e79336f45d90be735e4cecb1b0bdad
Ryan Camp <campryan@comcast.net>
2020-06-09 16:12:01 -0600

    Create boolean in road trips to check for api-key

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::RoadTripsController < ApplicationController
   def create
-    road_trip = RoadTrip.new(road_trip_params)
-    render json: RoadTripSerializer.new(road_trip)
+    if User.find_by(api_key: params[:api_key])
+      road_trip = RoadTrip.new(road_trip_params)
+      render json: RoadTripSerializer.new(road_trip)
+    else
+      error = Error.new(api_key_error)
+      render json: ErrorSerializer.new(error), status: :unauthorized
+    end
   end
 
   private
 
   def road_trip_params
     params.permit(:origin, :destination)
+  end
+
+  def api_key_error
+    'The API-Key you used is invalid'
   end
 end
