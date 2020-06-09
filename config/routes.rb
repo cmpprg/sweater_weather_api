5a8500b8caa080cd4f89f21b644f481a58f93320
Ryan Camp <campryan@comcast.net>
2020-06-08 19:56:40 -0600

    Create route for sessions create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resource :forecast, only: [:show]
       resource :backgrounds, only: [:show]
       resource :users, only: [:create]
+      resource :sessions, only: [:create]
     end
   end
 end
