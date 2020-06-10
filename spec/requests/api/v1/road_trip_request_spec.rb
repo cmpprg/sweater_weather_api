b44011a412dba992efb42b5b14103a0650279449
Ryan Camp <campryan@comcast.net>
2020-06-10 08:31:39 -0600

    Delete bad test

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -41,41 +41,6 @@ RSpec.describe 'Road Trip API endpoint' do
     end
   end
 
-  it 'can take origin, destination, and api-key and return trip info json above 30 min' do
-    VCR.use_cassette('roadtrip_req_happy_2') do
-      road_trip_params = {
-        origin: 'Denver,CO',
-        destination: 'Pueblo,CO',
-        api_key: @user.api_key
-      }
-
-      post '/api/v1/road_trip', params: road_trip_params.to_json, headers: @headers
-
-      expect(response).to be_successful
-
-      json = hash_json(response.body)
-
-      expect(json[:data]).to have_key(:id)
-      expect(json[:data]).to have_key(:type)
-      expect(json[:data]).to have_key(:attributes)
-
-      expect(json[:data][:attributes]).to have_key(:origin)
-      expect(json[:data][:attributes]).to have_key(:destination)
-      expect(json[:data][:attributes]).to have_key(:travel_time)
-      expect(json[:data][:attributes]).to have_key(:arrival_forecast)
-
-      expect(json[:data][:attributes][:arrival_forecast]).to have_key(:temperature)
-      expect(json[:data][:attributes][:arrival_forecast]).to have_key(:summary)
-
-      expect(json[:data][:attributes][:origin]).to_not be_nil
-      expect(json[:data][:attributes][:destination]).to_not be_nil
-      expect(json[:data][:attributes][:travel_time]).to_not be_nil
-
-      expect(json[:data][:attributes][:arrival_forecast][:temperature]).to_not be_nil
-      expect(json[:data][:attributes][:arrival_forecast][:summary]).to_not be_nil
-    end
-  end
-
   it 'can error if api-key is bad' do
     road_trip_params = {
       origin: 'Denver,CO',
