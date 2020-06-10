729c86eac7238690cd4252a7bcb4949690dab891
Ryan Camp <campryan@comcast.net>
2020-06-10 08:06:23 -0600

    Change method name for readablity

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -1,11 +1,11 @@
 class DailyWeather
   def initialize(weather_info)
-    @days = collection_of_weather_days(weather_info)
+    @days = collection_of_five_weather_days(weather_info)
   end
 
   private
 
-  def collection_of_weather_days(info)
+  def collection_of_five_weather_days(info)
     info[1..5].map { |day| WeatherDay.new(day) }
   end
 end
